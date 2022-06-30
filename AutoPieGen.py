d7d4a4388f8294cd96269dd9700e82a0d9cefcda
MaybeEther <75216074+MaybeEther@users.noreply.github.com>
2022-06-30 14:11:06 +0400

    not needed

--- AutoPieGen.py
+++ AutoPieGen.py
@@ -2,7 +2,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import xlrd
 ##########################################
-mycolors = [""] # can be used by user if wanted
 x = []
 a = []
 temp = ("test.xls") # file selecter
